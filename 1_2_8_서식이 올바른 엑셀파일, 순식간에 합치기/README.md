0e547c9bd25f1d60bf2b57064144bd70f301a509
needleworm <bhban@kakao.com>
2020-02-13 11:20:32 +0900

    예제 코드 완성

--- 1_2_8_서식이 올바른 엑셀파일, 순식간에 합치기/README.md
+++ 1_2_8_서식이 올바른 엑셀파일, 순식간에 합치기/README.md
@@ -2,14 +2,8 @@
 
 ### 아래 명령어를 입력하시면 코드가 실행됩니다.
 
->python sorter.py <TEMPLATE\> <DIRECTORY\> <MODE\>
+>python sorter.py <TEMPLATE\> <DIRECTORY\>
 
 <TEMPLATE\> 에는 양식 견본이 될 엑셀파일 이름을 적어줍니다.
 
 <DIRECTORY\> 에는 분석 대상 폴더를 입력합니다.
-
-<MODE\> 에는 어떤 모드로 작동시킬 것인지 적어줍니다.
-
-delete : 서식이 잘못된 파일 삭제
-report : 서식이 잘못된 파일 파일명 보고
-seperate : 서식이 잘못된 파일을 다른 폴더로 이동
--- 1_2_8_서식이 올바른 엑셀파일, 순식간에 합치기/merge_correct_xlxs.py
+++ 1_2_8_서식이 올바른 엑셀파일, 순식간에 합치기/merge_correct_xlxs.py
@@ -21,26 +21,14 @@ template = sys.argv[1]
 # 서식을 망가뜨릴 파일들이 저장된 폴더 이름을 시스템으로부터 입력받습니다.
 directory = sys.argv[2]
 
-# 작동 모드를 시스템으로부터 입력받습니다.
-MODE = sys.argv[3]
-
 # 폴더의 내용물을 열람해 목록을 생성합니다.
 input_files = os.listdir(directory)
 
 # 템플릿 파일을 읽어와 헤더를 분리합니다.
 HEADER = px.get_array(file_name=template)[0]
 
-# 삭제 모드인 경우 총 몇개의 파일이 삭제되었는지 세기 위해 카운터를 만듭니다.
-if MODE in "DELETEdelete":
-    count = 0
-# 보고 모드일 경우 보고서 작성을 위한 파일을 생성합니다.
-elif MODE in "REPORTreport":
-    REPORT = open("report.txt", 'w')
-# 분류 모드일 경우 분류된 파일을 저장하기 위한 폴더를 생성합니다.
-elif MODE in "SEPARATEseparate":
-    os.mkdir("wrong_files")
-    # 파일 이동을 수행하기 위한 라이브러리도 불러옵니다.
-    import shutil
+# 데이터를 저장할 리스트를 만듭니다. 헤더도 넣어줍니다.
+CONTENTS = [HEADER]
 
 # input_files에 저장된 파일 이름을 한 번에 하나씩 불러옵니다.
 for filename in input_files:
@@ -55,29 +43,18 @@ for filename in input_files:
     header = file[0]
 
     # 불러온 파일의 헤더가 템플릿과 일치하는지 분석합니다.
-    if HEADER == header:
-        # 일치한다면 살려둡시다.
+    if HEADER != header:
+        # 일치하지 않는다면 건너뛰어버립시다
         continue
 
-    # 일치하지 않는다면 이 아래 부분의 코드가 작동됩니다.
-    if MODE in "DELETEdelete":
-        # 삭제 모드인 경우 삭제합니다.
-        os.remove(directory + "/" + filename)
-        count += 1
-    elif MODE in "REPORTreport":
-        # 보고 모드인 경우 보고서에 파일 이름을 기재합니다.
-        REPORT.write(filename + "\n")
-    elif MODE in "SEPARATEseparate":
-        # 분류 모드일 경우 별도의 폴더로 파일을 이동시킵니다.
-        shutil.move(directory + "/" + filename, "wrong_files/" + filename)
+    # CONTENTS 리스트에 엑셀 파일의 내용물을 입력합니다.
+    CONTENTS += file[1:]
 
-# 삭제 모드인 경우 총 몇개의 파일이 삭제되었는지 출력합니다.
-if MODE in "DELETEdelete":
-    print("Total " + str(count) + " files were removed.")
+# 합쳐진 엑셀 파일을 저장합니다.
+px.save_as(array=CONTENTS, dest_file_name="merged_FILE.xlsx")
 
-# 보고 모드인 경우 보고서를 종료합니다.
-if MODE in "REPORTreport":
-    REPORT.close()
+# 총 몇개의 파일이 합쳐졌는지를 출력합니다.
+print("Total " + str(len(CONTENTS) - 1) + " files were merged.")
 
 # 작업 종료 메시지를 출력합니다.
 print("Process Done.")
