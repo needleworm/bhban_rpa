cbf80f954c172be09d54de22baf1f4b4757ea9fc
needleworm <bhban@kakao.com>
2020-03-03 11:51:46 +0900

    update

--- 3_2_3_여러 계정으로 번갈아가면서 트위터에 글 쓰기/main.py
+++ 3_2_3_여러 계정으로 번갈아가면서 트위터에 글 쓰기/main.py
@@ -17,13 +17,10 @@ print("Process Start.")
 start_time = time.time()
 
 # 아이디를 입력받습니다.
-id = sys.argv[1]
-
-# 패스워드를 입력받습니다.
-ps = sys.argv[2]
+idfile = sys.argv[1]
 
 # 트윗할 내용들이 적힌 파일을 입력받습니다.
-filename = sys.argv[3]
+filename = sys.argv[2]
 
 # 트윗 입력 x좌표를 입력받습니다.
 mention_x = int(sys.argv[4].strip())
@@ -31,17 +28,37 @@ mention_x = int(sys.argv[4].strip())
 # 트윗 입력 y좌표를 입력받습니다.
 mention_y = int(sys.argv[5].strip())
 
+# 아이디와 비밀번호를 세트로 저장해 둘 리스트를 만듭니다.
+IDs = []
+
+# 아이디가 기재된 파일을 불러옵니다.
+idfile = open(filename)
+
+# 이걸 한 줄씩 읽어옵니다.
+for line in idfile:
+    # 각 줄을 컴마로 쪼개 줍니다.
+    splt = line.split(",")
+    # 내용물이 두개가 아닌 라인은 모두 날려줍니다.
+    if len(splt) != 2:
+        continue
+    # IDs에 아이디와 비밀번호를 저장합니다.
+    IDs.append((splt[0].strip(), splt[1].strip()))
+
 # 크롤러를 불러옵니다.
 BOT = ta.TwitterBot(filename, (mention_x, mention_y))
 
-# 로그인을 시도합니다.
-BOT.login(id, ps)
-
-# 로그인에 성공했으니 스크린샷이나 한 번 찍어줍시다.
-BOT.save_screenshot(str(time.time) + ".png")
-
-# 트위터에 모든 멘션을 올립니다.
-BOT.tweet_all()
+# IDs에 저장된 계정을 하나씩 불러옵니다.
+for ids in IDs:
+    # 로그인을 시도합니다.
+    BOT.login(ids)
+    # 로그인에 성공했으니 스크린샷이나 한 번 찍어줍시다.
+    BOT.save_screenshot(str(time.time) + ".png")
+    # 트위터에 모든 멘션을 올립니다.
+    BOT.tweet_all()
+    # 크롤러를 닫아줍니다.
+    BOT.kill()
+    # 크롤러를 다시 켜서 트위터로 접속합니다.
+    BOT.go_to_twitter()
 
 # 결과 화면을 잠시 감상하기 위해 10초동안 방치합니다.
 time.sleep(10)
