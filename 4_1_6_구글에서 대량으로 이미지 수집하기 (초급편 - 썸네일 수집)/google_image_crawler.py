4a03e92e0db4900fa3a8086314100945df0a09dc
needleworm <bhban@kakao.com>
2020-03-05 16:50:22 +0900

    예제 업데이트

---
+++ 4_1_6_구글에서 대량으로 이미지 수집하기 (초급편 - 썸네일 수집)/google_image_crawler.py
@@ -0,0 +1,79 @@
+#-*-coding:euc-kr
+"""
+Author : Byunghyun Ban
+Book : 일반인을 위한 업무 자동화
+Last Modification : 2020.03.02.
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class ImgCrawler:
+    def __init__(self, out_dir):
+        # 쿼리 베이스를 제작합니다.
+        self.querry ="https://www.google.co.in/search?tbm=isch&q="
+        # 셀레늄 웹드라이버에 입력할 옵션을 지정합니다.
+        self.options = Options()
+        # 옵션에 해상도를 입력합니다.
+        #self.options.add_argument("--window-size=1024,768")
+        # 크롬 웹드라이버를 불러옵니다.
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.options)
+        # 결과물을 저장할 디렉터리를 기록합니다.
+        self.out_dir = out_dir
+
+    # 크롤러를 종료하는 메서드입니다.
+    # 굳이 한줄짜리 코드를 함수로 만든 데에는 여러 이유가 있습니다만,
+    # 쉽게 설명하자면 클래스 외부에서 클래스 내부 자료에 너무 깊게 관여하는 상황을 원하지 않기 때문입니다.
+    def kill(self):
+        self.driver.quit()
+
+    # 스크린샷을 저장하는 함수입니다.
+    def save_screenshot(self, filename):
+        self.driver.save_screenshot(filename)
+
+    # 키워드를 구글 이미지에서 검색하는 함수입니다.
+    def search_image(self, keyword):
+        self.driver.get(self.querry + keyword)
+        # 로딩이 오래 걸릴 수 있으니 잠시 대기합니다.
+        time.sleep(5)
+
+    # 이미지 검색 화면을 스크롤 다운하는 함수입니다.
+    def scroll_down(self):
+        # 사이트의 뼈대인 <body> 태그를 찾습니다.
+        body = self.driver.find_element_by_tag_name("body")
+        # 대충 많이 스크롤질 합시다.
+        for i in range(50):
+            body.send_keys(Keys.END)
+            time.sleep(0.5)
+        # 다시 맨 위로 올라갑시다. 기분 좋으라고 넣는겁니다. 없어도 됩니다.
+        body.send_keys(Keys.HOME)
+
+    # 이미지 검색 결과창의 모든 이미지를 긁어오는 함수입니다.
+    def crawl_pictures(self, num):
+        # 구글 검색은 초기에 이미지 검색 결과를 일부만 표기합니다.
+        # 스크롤을 맨 아래까지 내려서 사진을 추가로 로드합니다.
+        # 저자의 컴퓨터 기준으로 총 400개의 사진이 표기됩니다.
+        self.scroll_down()
+        # <img> 태그를 갖고있는 모든 요소를 불러옵시다.
+        img_elements = self.driver.find_elements_by_tag_name("img")
+        # for 문을 이용해 위 요소들을 하나하나 다운받습니다.
+        for i, el in enumerate(img_elements):
+            # 초기 세팅한 개수만큼 사진을 다운받았다면 루프를 끝내 줍니다.
+            if i == num:
+                break
+            # 화면이 신나게 번쩍이면서 다운로드됩니다.
+            el.screenshot(self.out_dir + "/" + str(i) + ".png")
+            time.sleep(0.1)
+        # 요청받은 개수보다 사진을 더 적게 다운받았다면
+        if i < num:
+            # 키워드를 바꿔서 다시 검색해 달라는 메시지를 출력합니다.
+            print("Not enough search result. Please change the keyword and try again.")
+
+    # 코드 간소화를 위해 자기가 알아서 구글 검색하고, 이미지 크롤링하는 함수를 만듭니다.
+    def crawl_images(self, keyword, num):
+        # 이미지 검색을 수행합니다.
+        self.search_image(keyword)
+        # 작업을 수행합니다.
+        self.crawl_pictures(num)
---
+++ 4_1_8_구글에서 대량으로 이미지 수집하기 (심화편 - 고화질 사진 캡처)/google_image_crawler.py
@@ -0,0 +1,104 @@
+#-*-coding:euc-kr
+"""
+Author : Byunghyun Ban
+Book : 일반인을 위한 업무 자동화
+Last Modification : 2020.03.02.
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+class ImgCrawler:
+    def __init__(self, out_dir):
+        # 쿼리 베이스를 제작합니다.
+        self.querry ="https://www.google.co.in/search?tbm=isch&q="
+        # 셀레늄 웹드라이버에 입력할 옵션을 지정합니다.
+        self.options = Options()
+        # 옵션에 해상도를 입력합니다.
+        #self.options.add_argument("--window-size=1024,768")
+        # 크롬 웹드라이버를 불러옵니다.
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.options)
+        # 결과물을 저장할 디렉터리를 기록합니다.
+        self.out_dir = out_dir
+
+    # 크롤러를 종료하는 메서드입니다.
+    # 굳이 한줄짜리 코드를 함수로 만든 데에는 여러 이유가 있습니다만,
+    # 쉽게 설명하자면 클래스 외부에서 클래스 내부 자료에 너무 깊게 관여하는 상황을 원하지 않기 때문입니다.
+    def kill(self):
+        self.driver.quit()
+
+    # 스크린샷을 저장하는 함수입니다.
+    def save_screenshot(self, filename):
+        self.driver.save_screenshot(filename)
+
+    # 키워드를 구글 이미지에서 검색하는 함수입니다.
+    def search_image(self, keyword):
+        self.driver.get(self.querry + keyword)
+        # 로딩이 오래 걸릴 수 있으니 잠시 대기합니다.
+        time.sleep(5)
+
+    # 이미지 검색 화면에서 첫 번째 이미지를 클릭해 미리보기 창을 띄우는 함수입니다.
+    def select_picture(self):
+        # 구글 검색의 이미지들은 <img> 태그로 감싸져 있습니다. 이 중 맨 위의 태그를 골라버립시다.
+        picture_element = self.driver.find_element_by_tag_name("img")
+        # 클릭합니다. 확대 이미지 창이 뜰겁니다.
+        picture_element.click()
+        # 5초 기다립니다.
+        time.sleep(5)
+
+    # 이미지 검색창의 확대샷이 열린 상태에서 이미지를 저장하고, 다음 사진으로 넘어가는 함수입니다.
+    def crawl_one_image(self):
+        # 확대된 이미지에서 이미지 요소를 뽑아옵니다.
+        # 이 요소의 xpath는 '//*[@id="Sva75c"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[2]/a/img' 입니다.
+        img_xpath = '//*[@id="Sva75c"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[2]/a/img'
+        # 이미지 요소를 가져옵니다.
+        image_element = self.driver.find_element_by_xpath(img_xpath)
+        # 이미지에서 원본 출처 링크를 뽑아냅니다.
+        image_url = image_element.get_attribute("src")
+        # 이미지만 새 창에 따로 불러와서 작업합시다.
+        # 드라이버 새 탭을 엽니다.
+        self.driver.execute_script("window.open('');")
+        # 새 탭은 driver.window_handles 안에 리스트로 저장됩니다.
+        # 나중에 추가된 탭일수록 리스트의 뒤에 추가됩니다.
+        # 리스트의 맨 뒤의 탭을 가져옵니다.
+        new_tab = self.driver.window_handles[-1]
+        # 새 탭으로 이동합니다.
+        self.driver.switch_to.window(new_tab)
+        # 이미지 링크로 이동합니다.
+        self.driver.get(image_url)
+        # 로딩 되기까지 좀 기다립니다.
+        time.sleep(5)
+        # 큰 이미지가 창에 떠 있습니다. 이 창에서 이미지 태그만 긁어옵시다.
+        image = self.driver.find_element_by_tag_name("img")
+        # 이미지를 저장합니다.
+        image.screenshot(self.out_dir + "/" + str(time.time()) + ".png")
+        # 볼 일이 끝났으니 이 탭은 닫아줍니다.
+        self.driver.close()
+        # 원래 탭으로 돌아옵니다.
+        self.driver.switch_to.window(self.driver.window_handles[0])
+
+    # 다음 이미지로 넘어가는 함수입니다.
+    def next_image(self):
+        # 다음 이미지로 넘어가기 위해 다음 버튼을 찾습니다.
+        # 이 버튼의 xpath는 '//*[@id="Sva75c"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[1]/a[2]/div' 입니다.
+        button_xpath = '//*[@id="Sva75c"]/div/div/div[3]/div[2]/div/div[1]/div[1]/div/div[1]/a[2]/div'
+        # 버튼 요소를 가져옵시다.
+        next_button = self.driver.find_element_by_xpath(button_xpath)
+        # 버튼을 눌러 다음 이미지로 넘어갑니다.
+        next_button.click()
+        # 로딩을 위해 잠시 기다립니다.
+        time.sleep(2)
+
+    # 코드 간소화를 위해 자기가 알아서 구글 검색하고, 이미지 크롤링하는 함수를 만듭니다.
+    def crawl_images(self, keyword, num=100):
+        # 이미지 검색을 수행합니다.
+        self.search_image(keyword)
+        # 첫 번째 이미지를 눌러 확대창을 켭니다.
+        self.select_picture()
+        # num 번 만큼 반복하며 이미지를 저장합니다.
+        for i in range(num):
+            # 이미지를 저장하고
+            self.crawl_one_image()
+            # 다음 이미지로 넘어갑니다.
+            self.next_image()
