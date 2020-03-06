9516a2df7f230a0b244cc5253a4f9feec793523c
needleworm <bhban@kakao.com>
2020-03-06 09:50:23 +0900

    다 됐다 완성 예제 완성 예아

---
+++ 4_3_9_멀티 계정 헤드리스 SNS 봇 - 인스타그램 댓글 봇/insta_bot_reply.py
@@ -0,0 +1,143 @@
+#-*-coding:euc-kr
+"""
+Author : Byunghyun Ban
+Book : 일반인을 위한 업무 자동화
+Last Modification : 2020.03.02.
+"""
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+
+class ReplyBot:
+    def __init__(self, replyfile):
+        # 쿼리 베이스를 제작합니다.
+        self.querry ="https://www.instagram.com/explore/tags/"
+        # 셀레늄 웹드라이버에 입력할 옵션을 지정합니다.
+        self.options = Options()
+        # 옵션에 헤드리스를 명시합니다. 이러면 헤드리스로 작업이 수행됩니다.
+        self.options.add_argument("headless")
+        # 헤드리스 브라우저의 창 크기를 지정해 줍니다.
+        self.options.add_argument("--window-size=848,1500")
+        # 크롬 웹드라이버를 불러옵니다.
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.options)
+        # 댓글 내용이 적힌 파일을 불러옵니다. 인코딩이 utf8이 아니면 바꿔주세요.
+        self.replyfile = open(replyfile, encoding="utf8")
+        # 댓글 내용 리스트를 만듭니다.
+        self.replylist = self.replyfile.read().split("\n")
+
+    # 크롤러를 종료하는 메서드입니다.
+    # 굳이 한줄짜리 코드를 함수로 만든 데에는 여러 이유가 있습니다만,
+    # 쉽게 설명하자면 클래스 외부에서 클래스 내부 자료에 너무 깊게 관여하는 상황을 원하지 않기 때문입니다.
+    def kill(self):
+        self.driver.quit()
+
+    # 스크린샷을 저장하는 함수입니다.
+    def save_screenshot(self, filename):
+        self.driver.save_screenshot(filename)
+
+    # 인스타그램 로그인 함수입니다.
+    def login(self, id, ps):
+        # 로그인 페이지로 이동합니다.
+        self.driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+        # 로딩을 위해 10초 정도 기다려 줍니다.
+        time.sleep(10)
+        # ID, PS 입력 요소는 <input> 태그입니다. 요소를 찾아줍시다.
+        input_field = self.driver.find_elements_by_tag_name("input")
+        # 첫 번째 요소가 아이디입니다. 아이디를 입력합니다.
+        input_field[0].send_keys(id)
+        # 비밀번호 입력 요소는 두 번째입니다. 비밀번호를 입력합니다.
+        input_field[1].send_keys(ps)
+        # 엔터키를 쳐서 로그인을 마무리합니다.
+        input_field[1].send_keys(Keys.RETURN)
+        # 10초 정도 기다려 줍니다.
+        time.sleep(10)
+
+    # 인스타그램에서 태그를 검색하는 함수입니다.
+    def search_tag(self, tag):
+        self.driver.get(self.querry + tag)
+        # 로딩이 오래 걸릴 수 있으니 잠시 대기합니다.
+        time.sleep(5)
+
+    # 태그 검색 화면에서 최근에 게시된 첫 번째 사진을 골라 클릭합니다.
+    def select_picture(self):
+        # 최근 사진의 xpath는 아래와 같습니다.
+        recent_picture_xpath = '//*[@id="react-root"]/section/main/article/div[2]/div/div[1]/div[1]'
+        # 최근 사진의 요소를 가져옵니다.
+        recent_picture = self.driver.find_element_by_xpath(recent_picture_xpath)
+        # 최근 사진을 클릭합니다.
+        recent_picture.click()
+
+    # 검색결과들을 돌아다니며 모조리 좋아요를 누르고 댓글도 답니다.
+    # num에는 몇 개의 게시물을 좋아요 할지 입력합니다.
+    # -1을 입력하면 사용자가 직접 종료하기 전까지 무한정 계속합니다.
+    def press_like_and_reply(self, num):
+        # 반복 회수를 결정하기 위한 변수입니다.
+        count = num
+        # count 가 1개씩 깎이면서, 0이 될때까지 반복합니다.
+        # num이 -1인 경우 계속 0보다 작아지기만 하고 0이 되지는 않으므로 영원히 실행됩니다.
+        while count != 0:
+            # 좋아요 버튼의 태그를 직접 찾기는 힘듭니다. 좋아요 버튼은 <svg> 태그로 만들어져 있는데
+            # 이 화면에 <svg> 태그를 갖고 있는 버튼이 한 두 개가 아닙니다.
+            # 그러므로 일단 <svg> 태그를 가진 요소를 몽땅 갖고옵니다.
+            svg = self.driver.find_elements_by_tag_name("svg")
+            # <svg> 태그는 내부에 aria-label 이라는 이름의 어트리뷰트를 갖고 있습니다.
+            # 이 어트리뷰트가 '좋아요' 인 svg요소만 찾아내 클릭합시다.
+            # for 문으로 일단 svg 태그들을 모조리 불러옵니다.
+            for el in svg:
+                # 태그 내부의 aria-label 어트리뷰트가 좋아요 인 경우만 잡아냅니다.
+                # 이미 좋아요가 눌려져 있는 경우 어트리뷰트 값이 "좋아요 취소" 로 변경됩니다.
+                # 따라서 이 방법은 이미 좋아요를 눌러 둔 게시물은 건너뛸 수 있다는 장점도 가집니다.
+                # 이미 좋아요를 누른 게시물은 건너뜁니다.
+                if el.get_attribute("aria-label") == "좋아요 취소":
+                    break
+                # 좋아요 버튼을 찾습니다.
+                if el.get_attribute("aria-label") == "좋아요":
+                    # 좋아요 버튼을 클릭합니다.
+                    el.click()
+                    # 적당히 오래 기다려 줍니다.
+                    time.sleep(10)
+                    # 댓글 파일 중 랜덤으로 하나를 뽑아서 댓글을 달아줍시다.
+                    # 에러가 나면 두 번 더 시도합니다.
+                    # 어지간해선 3번이면 됩니다.
+                    # 이렇게 try except문을 겹쳐 쓰는건 원래는 절대 하지 말아야 할 코딩 방법입니다.
+                    try:
+                        self.send_reply(random.choice(self.replylist))
+                    except:
+                        time.sleep(5)
+                        try:
+                            self.send_reply(random.choice(self.replylist))
+                        except:
+                            time.sleep(5)
+                            self.send_reply(random.choice(self.replylist))
+                    # 카운트를 한 개씩 깎아내립니다.
+                    count -= 1
+                    break
+            # 다음 게시물로 넘어갑시다. 다음 버튼에는 link text가 "다음"으로 기재되어 있습니다. 요소를 찾습니다.
+            next_button = self.driver.find_element_by_link_text("다음")
+            # 클릭합니다.
+            next_button.click()
+            # 댓글은 좀 긴 텀을 두고 달아야 합니다. 안 그러면 인스타 운영진들이 댓글 개제 중단 재제를 줍니다.
+            time.sleep(60)
+
+    # 댓글을 남기는 함수입니다.
+    def send_reply(self, text):
+        # 댓글 입력창은 <textarea> 라는 태그로 되어 있습니다.
+        textarea = self.driver.find_elements_by_tag_name("textarea")
+        # 태그를 위에서부터 읽어 옵니다. 댓글 입력창은 aria-label 이라는 어트리뷰트에 "댓글 달기..." 라고 적혀 있습니다.
+        for el in textarea:
+            if el.get_attribute("aria-label") == "댓글 달기...":
+                # 댓글 입력창에 댓글을 적어 줍시다. 댓글과 함께 엔터키도 입력해 바로 댓글을 올립니다.
+                el.send_keys(text, Keys.RETURN)
+                break
+
+    # 코드 간소화를 위해 자기가 알아서 인스타 로그인하고, 검색하고, 캡처도 다 하는 메서드를 만듭시다.
+    def insta_jungdok(self, tag, num=100):
+        # 태그도 검색하고
+        self.search_tag(tag)
+        # 사진 한 장을 선택한 다음
+        self.select_picture()
+        # 좋아요를 누르고, 댓글을 달면서 사진을 한 장씩 넘겨줍니다.
+        self.press_like_and_reply(num)
---
+++ 4_3_9_멀티 계정 헤드리스 SNS 봇 - 인스타그램 댓글 봇/main.py
@@ -0,0 +1,68 @@
+#-*-coding:euc-kr
+"""
+Author : Byunghyun Ban
+Book : 일반인을 위한 업무 자동화
+Last Modification : 2020.03.02.
+"""
+
+import sys
+import time
+import insta_bot_reply as ib
+
+
+# 작업 시작 메시지를 출력합니다.
+print("Process Start.")
+
+# 시작 시점의 시간을 기록합니다.
+start_time = time.time()
+
+# 계정 정보가 입력된 파일 이름을 입력받습니다.
+id_filename = sys.argv[1]
+
+# 검색할 태그가 기록된 파일을 입력받습니다.
+tag_filename = sys.argv[2]
+
+# 크롤러를 불러옵니다.
+BOT = ib.LikeBot()
+
+# id 파일을 불러옵니다.
+id_file = open(id_filename)
+# 리스트로 만들어 줍니다.
+IDs = []
+for line in id_file:
+    splt = line.split(",")
+    if len(splt) != 2:
+        continue
+    IDs.append((splt[0].strip(), splt[1].strip()))
+
+# 태그 파일도 불러옵니다.
+tag_file = open(tag_filename)
+
+# 리스트로 만들어 줍니다.
+Tags = tag_file.read().split("\n")
+
+# 무한반복 할 때에는 while True가 최고입니다.
+while True:
+    # 계정을 하나씩 불러옵니다.
+    for account in IDs:
+        # 계정이 바뀌었으니 드라이버를 껐다가 켜 줍니다.
+        BOT.driver_refresh()
+        # 로그인을 합시다.
+        BOT.login(account)
+        # 태그를 하나씩 불러옵니다.
+        for tag in Tags:
+            # 불러온 태그를 대상으로 탐색하며 좋아요를 누릅니다. 게시물은 한 번에 100개씩만 좋아요 누르고 댓글 답니다.
+            BOT.insta_jungdok(tag, num=100)
+        # 계정이 바뀔 때마다 1분씩 기다려 줍니다.
+        # 동일한 멘션을 너무 자주 입력하면 밴을 당하기 때문입니다.
+        time.sleep(60)
+
+# 크롤러를 닫아줍니다.
+BOT.kill()
+
+# 작업 종료 메세지를 출력합니다.
+print("Process Done.")
+
+# 작업에 총 몇 초가 걸렸는지 출력합니다.
+end_time = time.time()
+print("The Job Took " + str(end_time - start_time) + " seconds.")
