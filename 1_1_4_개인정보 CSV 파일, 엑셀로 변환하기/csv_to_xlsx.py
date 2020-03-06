ae6810a7a6209298448fa28c6327ecb6a15a31b3
needleworm <bhban@kakao.com>
2020-03-06 16:55:33 +0900

    예제 4번

--- 1_1_4_개인정보 CSV 파일, 엑셀로 변환하기/csv_to_xlsx.py
+++ 1_1_4_개인정보 CSV 파일, 엑셀로 변환하기/csv_to_xlsx.py
@@ -17,9 +17,10 @@ print("Process Start")
 # 시작 시점의 시간을 기록합니다.
 start_time = time.time()
 
-
 # 터미널에서 인자를 입력받기 위한 코드입니다.
+# 엑셀로 변환하고자 하는 CSV 파일의 이름을 입력합니다.
 input_file = sys.argv[1]
+# 합쳐진 결과물 파일을 어떤 이름으로 저장할지 입력받습니다.
 result_file = sys.argv[2]
 
 # 엑셀 파일 하나로 합쳐주는 함수입니다.
