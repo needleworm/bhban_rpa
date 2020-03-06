c26c4c6b8dadd3304e79e55f4ddf21ac85e27855
needleworm <bhban@kakao.com>
2020-03-06 10:58:18 +0900

    예제 42번

--- 4_1_2_멀티 계정 SNS 봇 - 트위터 봇/twitter_bot_multi.py
+++ 4_1_2_멀티 계정 SNS 봇 - 트위터 봇/twitter_bot_multi.py
@@ -1,6 +1,8 @@
+#-*-coding:euc-kr
 """
 Author : Byunghyun Ban
-Book : 일반인을 위한 업무 자동화
+GitHub : https://github.com/needleworm
+Book : �Ϲ����� ���� ���� �ڵ�ȭ
 Last Modification : 2020.03.02.
 """
 from selenium import webdriver
@@ -11,70 +13,70 @@ import time
 
 class TwitterBot:
     def __init__(self):
-        # 셀레늄 웹드라이버에 입력할 옵션을 지정합니다.
+        # ������ ������̹��� �Է��� �ɼ��� �����մϴ�.
         self.options = Options()
-        # 옵션에 해상도를 입력합니다.
+        # �ɼǿ� �ػ󵵸� �Է��մϴ�.
         #self.options.add_argument("--window-size=1024,768")
-        # 옵션을 입력해서 크롬 웹드라이버를 불러옵니다.
+        # �ɼ��� �Է��ؼ� ũ�� ������̹��� �ҷ��ɴϴ�.
         self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.options)
-        # 트윗할 메시지들을 저장할 공간을 만듭니다.
+        # Ʈ���� �޽������� ������ ������ ����ϴ�.
         self.contents = []
 
-    # 크롤러를 종료하는 메서드입니다.
-    # 굳이 한줄짜리 코드를 함수로 만든 데에는 여러 이유가 있습니다만,
-    # 쉽게 설명하자면 클래스 외부에서 클래스 내부 자료에 너무 깊게 관여하는 상황을 원하지 않기 때문입니다.
+    # ũ�ѷ��� �����ϴ� �޼����Դϴ�.
+    # ���� ����¥�� �ڵ带 �Լ��� ���� ������ ���� ������ �ֽ��ϴٸ�,
+    # ���� �������ڸ� Ŭ���� �ܺο��� Ŭ���� ���� �ڷῡ �ʹ� ���� �����ϴ� ��Ȳ�� ������ �ʱ� �����Դϴ�.
     def kill(self):
         self.driver.quit()
 
-    # 로그인을 수행하는 메서드입니다.
+    # �α����� �����ϴ� �޼����Դϴ�.
     def login(self, id, ps):
-        # 트위터 로그인창으로 들어갑니다.
+        # Ʈ���� �α���â���� ���ϴ�.
         self.driver.get("https://twitter.com/login")
-        # 로딩이 오래 걸릴 수 있으니 잠시 대기합니다.
+        # �ε��� ���� �ɸ� �� ������ ��� ����մϴ�.
         time.sleep(3)
-        # 아이디를 입력하기 위해 아이디 입력창 요소를 찾아옵니다.
-        # 트위터의 경우 아이디 입력창은 session[username_or_email] 이라는 이름을 갖고 있습니다.
+        # ���̵� �Է��ϱ� ���� ���̵� �Է�â ��Ҹ� ã�ƿɴϴ�.
+        # Ʈ������ ��� ���̵� �Է�â�� session[username_or_email] �̶�� �̸��� ���� �ֽ��ϴ�.
         id_input = self.driver.find_element_by_name("session[username_or_email]")
-        # id를 입력합니다.
+        # id�� �Է��մϴ�.
         id_input.send_keys(id)
 
-        # 비밀번호를 입력합니다.
-        # 트위터의 경우 비밀번호 입력창은 session[password] 라는 이름을 갖고 있습니다.
+        # ��й�ȣ�� �Է��մϴ�.
+        # Ʈ������ ��� ��й�ȣ �Է�â�� session[password] ��� �̸��� ���� �ֽ��ϴ�.
         ps_input = self.driver.find_element_by_name("session[password]")
         ps_input.send_keys(ps)
         ps_input.send_keys(Keys.RETURN)
 
-    # 파일을 읽어와 트윗할 메시지를 정리하는 메서드입니다.
+    # ������ �о�� Ʈ���� �޽����� �����ϴ� �޼����Դϴ�.
     def prepare_contents(self, filename):
-        # 인코딩이 utf8이 아닐 경우 변경해주세요.
+        # ���ڵ��� utf8�� �ƴ� ��� �������ּ���.
         file = open(filename, encoding="utf8")
         self.contents = file.read().split("\n")
 
-    # 메시지를 입력받아 트윗하는 매서드입니다.
+    # �޽����� �Է¹޾� Ʈ���ϴ� �ż����Դϴ�.
     def tweet(self, string):
-        # 트윗 멘션을 쉽게 입력할 수 있게 전용 페이지로 이동합니다.
+        # Ʈ�� ����� ���� �Է��� �� �ְ� ���� �������� �̵��մϴ�.
         self.driver.get("https://twitter.com/intent/tweet")
-        # 메시지 입력창 요소를 찾습니다. id="status" 입니다.
+        # �޽��� �Է�â ��Ҹ� ã���ϴ�. id="status" �Դϴ�.
         board = self.driver.find_element_by_id("status")
-        # 메시지 입력창에 메시지를 보냅니다.
+        # �޽��� �Է�â�� �޽����� �����ϴ�.
         board.send_keys(string)
-        # Tweet 버튼 요소를 가져옵니다. 우선 하단 영역을 긁어옵시다. 하단 영역은 ft 라는 이름의 클래스입니다.
+        # Tweet ��ư ��Ҹ� �����ɴϴ�. �켱 �ϴ� ������ �ܾ�ɽô�. �ϴ� ������ ft ��� �̸��� Ŭ�����Դϴ�.
         lower_menu = self.driver.find_element_by_class_name("ft")
-        # 버튼은 lower_menu 내부에 input 이라는 태그로 감싸져 있습니다. 요소를 가져옵시다.
+        # ��ư�� lower_menu ���ο� input �̶�� �±׷� ������ �ֽ��ϴ�. ��Ҹ� �����ɽô�.
         button = lower_menu.find_element_by_tag_name("input")
-        # 버튼을 클릭합니다.
+        # ��ư�� Ŭ���մϴ�.
         button.click()
 
-    # self.contents에 저장된 모든 메시지를 하나씩 트윗하는 매서드입니다.
+    # self.contents�� ����� ��� �޽����� �ϳ��� Ʈ���ϴ� �ż����Դϴ�.
     def tweet_all(self, interval):
-        # for문을 사용해 모든 메시지를 하나씩 접근합니다.
+        # for���� ����� ��� �޽����� �ϳ��� �����մϴ�.
         for el in self.contents:
-            # 메시지를 하나씩 트윗합니다.
+            # �޽����� �ϳ��� Ʈ���մϴ�.
             self.tweet(el)
-            # 로딩에 좀 걸릴 수 있으므로 기다려줍니다.
+            # �ε��� �� �ɸ� �� �����Ƿ� ��ٷ��ݴϴ�.
             time.sleep(interval)
 
-    # 파일을 읽어온 다음, 모두 트윗하는 매서드입니다.
+    # ������ �о�� ����, ��� Ʈ���ϴ� �ż����Դϴ�.
     def twitter_jungdok(self, filename, interval=10):
         self.prepare_contents(filename)
         time.sleep(5)
