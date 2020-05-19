3fdca4cb4e6cfacd1e6359197c4540271a310ea1
needleworm <bhban@kakao.com>
2020-05-19 22:20:04 +0900

    여백수정

--- Part 1 컴퓨터 자동화 기초/Chapter 3. 컴퓨터! 디자인 작업도 자신 있지 1분 안에 모두 처리해!/1_3_6_예제를 위해 증명사진 1,000장이 필요해졌네요/augmentation.py
+++ Part 1 컴퓨터 자동화 기초/Chapter 3. 컴퓨터! 디자인 작업도 자신 있지 1분 안에 모두 처리해!/1_3_6_예제를 위해 증명사진 1,000장이 필요해졌네요/augmentation.py
@@ -32,8 +32,11 @@ Xdim, Ydim = image.size
 COUNT = 1
 
 # 일단 원본을 저장합니다. 2의 0승
+# 저장할 파일의 이름을 입력합니다.
 temp_new_file_name = "%05d.png" %COUNT
+# 카운트를 1 증가시킵니다.
 COUNT += 1
+# 원본 이미지를 저장합니다.
 image.save(out_dir + "/" + temp_new_file_name)
 image.close()
 
@@ -42,45 +45,70 @@ FILELIST = [temp_new_file_name]
 
 # 폴더 내의 이미지를 모두 읽어와 좌우대칭을 저장합니다. 2의 1승
 for i in range(len(FILELIST)):
+    # 파일을 불러옵니다.
     image = Image.open(out_dir + "/" + FILELIST[i])
+    # 변환된 파일을 저장하기 위해 새로운 이름을 지정합니다.
     new_temp_name = "%05d.png" %COUNT
+    # 사진이 한 장 만들어질때마다 count를 1씩 증가시킵니다.
     COUNT += 1
+    # 이미지를 좌우 반전합니다.
     image = image.transpose(Image.FLIP_LEFT_RIGHT)
+    # 좌우 반전된 이미지를 저장합니다.
     image.save(out_dir + "/" + new_temp_name)
     image.close()
+    # 출력 파일명을 리스트에 저장합니다.
     FILELIST.append(new_temp_name)
 
-# 폴더 내의 이미지를 모두 읽어와 상하대칭을 저장합니다. 2의 2승
+# 리스트 안의 이미지를 모두 읽어와 상하대칭을 저장합니다. 2의 2승
 for i in range(len(FILELIST)):
+    # 파일을 불러옵니다.
     image = Image.open(out_dir + "/" + FILELIST[i])
+    # 변환된 파일을 저장하기 위해 새로운 이름을 지정합니다.
     new_temp_name = "%05d.png" % COUNT
+    # 사진이 한 장 만들어질때마다 count를 1씩 증가시킵니다.
     COUNT += 1
+    # 이미지를 상하 반전합니다.
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    # 상하 반전된 이미지를 저장합니다.
     image.save(out_dir + "/" + new_temp_name)
     image.close()
+    # 출력 파일명을 리스트에 저장합니다.
     FILELIST.append(new_temp_name)
 
-# 폴더 내의 이미지를 모두 읽어와 흑백버전을 저장합니다. 2의 3승
+# 리스트 안의 이미지를 모두 읽어와 흑백버전을 저장합니다. 2의 3승
 for i in range(len(FILELIST)):
+    # 파일을 불러옵니다.
     image = Image.open(out_dir + "/" + FILELIST[i])
+    # 변환된 파일을 저장하기 위해 새로운 이름을 지정합니다.
     new_temp_name = "%05d.png" % COUNT
+    # 사진이 한 장 만들어질때마다 count를 1씩 증가시킵니다.
     COUNT += 1
+    # 이미지를 흑백으로 만듭니다.
     image = image.convert('1')
+    # 흑백으로 변환된 이미지를 저장합니다.
     image.save(out_dir + "/" + new_temp_name)
     image.close()
+    # 출력 파일명을 리스트에 저장합니다.
     FILELIST.append(new_temp_name)
 
-# 폴더 내의 이미지를 모두 읽어와 1도씩 회전합니다. 2의 3승 * 180
+# 리스트 안의 이미지를 모두 읽어와 1도씩 회전합니다. 2의 3승 * 180
 for el in FILELIST:
     for i in range(180):
         # 깔끔하게 1,000장만 만듭시다.
+        # 결과물이 1000개를 넘어서면 코드를 종료합니다.
         if COUNT > 1000:
             break
+        # 파일을 불러옵니다.
         image = Image.open(out_dir + "/" + el)
-        image = image.rotate(i+1)
-        image = image.resize((Xdim, Ydim))
+        # 변환된 파일을 저장하기 위해 새로운 이름을 지정합니다.
         new_temp_name = "%05d.png" % COUNT
+        # 사진이 한 장 만들어질때마다 count를 1씩 증가시킵니다.
         COUNT += 1
+        # 사진을 회전시킵니다.
+        image = image.rotate(i+1)
+        # 간혹 이미지 크기가 변경된다는 이야기가 있어 resize()를 실행합니다.
+        image = image.resize((Xdim, Ydim))
+        # 회전 된 이미지를 저장합니다.
         image.save(out_dir + "/" + new_temp_name)
         image.close()
 
