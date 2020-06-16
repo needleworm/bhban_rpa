393f30645e170c2aa016cafcf1ebaa4c85786b6e
needleworm <bhban@kakao.com>
2020-06-16 15:39:36 +0900

    simplify

--- Part 3 인터넷 활용 자동화/Chapter 2. 인터넷 자동화 초급 - 매크로 활용/3_2_1_매크로를 활용해 로그인 구현하기/login_macro.py
+++ Part 3 인터넷 활용 자동화/Chapter 2. 인터넷 자동화 초급 - 매크로 활용/3_2_1_매크로를 활용해 로그인 구현하기/login_macro.py
@@ -13,10 +13,7 @@ import time
 # 각종 사이트들의 로그인 주소를 미리 저장해 둔 딕셔너리입니다.
 LOGIN_URLS = {
     "twitter": "https://twitter.com/login",
-    "naver": "https://nid.naver.com/nidlogin.login?",
-    "google": "https://accounts.google.com/signin",
     "daum": "https://logins.daum.net/accounts/signinform.do",
-    "facebook": "https://facebook.com"
 }
 
 
@@ -25,7 +22,7 @@ class LoginBot:
         # 셀레늄 웹드라이버에 입력할 옵션을 지정합니다.
         self.options = Options()
         # 옵션에 해상도를 입력합니다.
-        #self.options.add_argument("--window-size=1024,768")
+        self.options.add_argument("--window-size=1600,900")
         # 옵션을 입력해서 크롬 웹드라이버를 불러옵니다.
         self.driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=self.options)
         # 로그인하려는 사이트로 이동해 로그인창을 켭니다.
