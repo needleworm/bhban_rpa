47f53865400c714bddab55293239a73599c9a04d
needleworm <bhban@kakao.com>
2023-01-09 14:59:01 +0900

    .

---
+++ app/src/main/java/com/example/rpa/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.rpa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView) // 웹뷰 객체 가져오기
+        webView.webViewClient = WebViewClient() // 웹뷰 클라이언트 생성
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://needleworm.github.io/bhban_rpa") // 해당 url 로딩
+
+        if (savedInstanceState != null) webView.restoreState(savedInstanceState)
+        else webView.loadUrl("https://needleworm.github.io/bhban_rpa")
+
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        webView.saveState(outState)
+    }
+
+}
